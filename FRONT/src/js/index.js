b1bf74903b9de8bc7f226b0d6c458c8299dfc2ed
JEAN LOUIS DOS SANTOS <jeanlouis.dossantos84@gmail.com>
2021-03-28 15:14:49 +0200

    Page d'accueil

--- FRONT/src/js/index.js
+++ FRONT/src/js/index.js
@@ -1,24 +1,35 @@
-import _ from 'lodash';
-import '../style/style.scss';
+import "../style/style.scss";
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
+console.log(backendurl);
+const url = "http://localhost:3000/api/teddies/";
+
+// let json = fetch(url).then(response => response.json());
+// console.log(json)
+const convertToCurrency = (price) => Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR', minimumFractionDigits:2 }).format(price)
+
+
+let request = new XMLHttpRequest();
+request.open("GET", url);
+request.send();
+request.onreadystatechange = function () {
+  if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+    var response = JSON.parse(this.responseText);
+    console.log(response[0]);
+
+    let test = document.querySelector("body > main > div");
+
+   
+response.forEach((element) => {
+
+
+      let card =
+        `<div id="articlecard1" class="w-2/5 border-4 rounded-md border-black m-8 ">
+         <img src="${element.imageUrl}" alt="">
+         <h2 class="text-center text-2xl">${element.name}</h2>
+         <p class="text-center text-2xl mt-8 mb-2"> A partir de <span class="text-bold">${convertToCurrency(element.price/100)}</span></p>
+         </div>`;
+
+     test.innerHTML += card;
+    });
   }
-function blueComponent(){
-  const element = document.createElement('div');
-  element.innerHTML = 'mon test à moi perso';
-  element.classList.add('goodbye');
-return element;
-}  
-  document.body.appendChild(component());
-  document.body.appendChild(blueComponent());
-
-
-let a;
-console.log(a);
+};
--- FRONT/webpack.config.js
+++ FRONT/webpack.config.js
@@ -1,38 +1,62 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const onbuildplugin = require("on-build-webpack");
+const {exec} = require("child_process");
+const webpack = require("webpack");
 
-module.exports = env => {
-  console.log(env)
-  return{
-  mode: env.mode,
-  entry: "./src/js/index.js",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: ".",
-      template : "src/views/template.html",
-    }),
-  ],
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+
+module.exports = (env) => {
+  console.log(env);
+  return {
+    mode: env.mode,
+    entry: "./src/js/index.js",
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Orinoco, site de vente en ligne",
+        template: "src/views/template.html",
+      }),
+      new HtmlWebpackPlugin({
+        title: "produit",
+        template: "src/views/template_produit.html",
+        filename: "product.html"
       }
+      
+      ),
+      new webpack.DefinePlugin({
+        backendurl : JSON.stringify("http://localhost:3000/api/teddies/")
+      })
+      
     ],
-  },
-}};
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          exclude: /node_modules/,
+          use: ["style-loader","css-loader","sass-loader", ],
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {loader: 'url-loader'}
+          ]
+        },
+
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
